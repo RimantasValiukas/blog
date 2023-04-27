adafd5e1eb461ad2420831cfda7eda6a6fc7e58d
RimantasValiukas <rimantas.valiukas@gmail.com>
2023-04-27 16:52:45 +0300

    GET method added, connected to database, form for article created

--- blog/src/main/java/lt/code/academy/blog/controller/ArticleController.java
+++ blog/src/main/java/lt/code/academy/blog/controller/ArticleController.java
@@ -1,7 +1,10 @@
 package lt.code.academy.blog.controller;
 
+import lt.code.academy.blog.dto.Article;
 import lt.code.academy.blog.service.ArticleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +15,13 @@ public class ArticleController {
 
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
+    }
+
+    @GetMapping("/create")
+    public String openArticleForm(Model model) {
+        model.addAttribute("article", new Article());
+
+        return "form/article";
     }
 
 
